d5496c3399c5c539fc24754814d6381758d5a6c2
Abdur Rahman <arwithlpd@gmail.com>
2022-12-01 15:28:00 +0600

    update

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
-import main from '../assets/images/6.jpg';
+import main from '../assets/images/filing-system.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
 import { useAppContext } from '../context/appContext';
@@ -18,12 +18,9 @@ const Landing = () => {
           {/* info */}
           <div className='info'>
             <h1>
-              job <span>tracking</span> app
+              applicant <span>tracking</span> system
             </h1>
-            <p>
-              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. Aesthetic post-ironic venmo, quinoa lo-fi tote bag
-              adaptogen everyday carry meggings +1 brunch narwhal.
-            </p>
+            <p>An applicant tracking system (ATS) is software for recruiters and employers to track candidates throughout the recruiting and hiring process.</p>
             <Link
               to='/register'
               className='btn btn-hero'
