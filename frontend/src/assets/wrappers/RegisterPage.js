1dc4f22f450adcb51d617af4f6a371a2ffb47c07
Abdur Rahman <arwithlpd@gmail.com>
2022-12-08 16:02:17 +0600

    color update

--- frontend/src/assets/wrappers/RegisterPage.js
+++ frontend/src/assets/wrappers/RegisterPage.js
@@ -1,35 +1,38 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Wrapper = styled.section`
-  display: grid;
-  align-items: center;
-  .logo {
-    display: block;
-    margin: 0 auto;
-    margin-bottom: 1.38rem;
-  }
-  .form {
-    max-width: 400px;
-    border-top: 5px solid var(--primary-500);
-  }
+    display: grid;
+    align-items: center;
+    .logo {
+        display: block;
+        margin: 0 auto;
+        margin-bottom: 1.38rem;
+    }
+    .form {
+        max-width: 400px;
+        border-bottom: 5px solid var(--primary-500);
+        border-left: 3px solid var(--primary-500);
+        border-radius: var(--borderRadius);
+        box-shadow: var(--shadow-2);
+    }
 
-  h3 {
-    text-align: center;
-  }
-  p {
-    margin: 0;
-    margin-top: 1rem;
-    text-align: center;
-  }
-  .btn {
-    margin-top: 1rem;
-  }
-  .member-btn {
-    background: transparent;
-    border: transparent;
-    color: var(--primary-500);
-    cursor: pointer;
-    letter-spacing: var(--letterSpacing);
-  }
-`
-export default Wrapper
+    h3 {
+        text-align: center;
+    }
+    p {
+        margin: 0;
+        margin-top: 1rem;
+        text-align: center;
+    }
+    .btn {
+        margin-top: 1rem;
+    }
+    .member-btn {
+        background: transparent;
+        border: transparent;
+        color: var(--primary-500);
+        cursor: pointer;
+        letter-spacing: var(--letterSpacing);
+    }
+`;
+export default Wrapper;
