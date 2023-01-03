ade7180836f0801543f385ac0e9c1be29f1eb9b3
Abdur Rahman <arwithlpd@gmail.com>
2023-01-03 13:54:43 +0600

    update

--- frontend/src/assets/wrappers/SearchContainer.js
+++ frontend/src/assets/wrappers/SearchContainer.js
@@ -1,51 +1,44 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.section`
-    border-radius: var(--borderRadius);
-    width: 30%;
-    background: var(--white);
-    box-shadow: var(--shadow-2);
-    h3 {
-        margin-top: 0;
-    }
-    .form {
-        width: 100%;
-        max-width: 100%;
-    }
-    .form-input,
-    .form-select,
-    .btn-block {
-        height: 35px;
-    }
-    .form-row {
-        margin-bottom: 0;
-    }
+  .form {
+    width: 100%;
+    max-width: 100%;
+  }
+  .form-input,
+  .form-select,
+  .btn-block {
+    height: 35px;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    column-gap: 2rem;
+    row-gap: 0.5rem;
+  }
+  h5 {
+    font-weight: 700;
+  }
+  .btn-block {
+    align-self: end;
+    margin-top: 1rem;
+  }
+  @media (min-width: 768px) {
     .form-center {
-        /* display: grid; */
-        grid-template-columns: 1fr;
-        column-gap: 2rem;
-        row-gap: 0.5rem;
+      grid-template-columns: 1fr 1fr;
     }
-    h5 {
-        font-weight: 700;
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
     }
     .btn-block {
-        align-self: end;
-        margin-top: 1rem;
+      margin-top: 0;
     }
-    @media (min-width: 768px) {
-        .form-center {
-            grid-template-columns: 1fr 1fr;
-        }
-    }
-    @media (min-width: 992px) {
-        .form-center {
-            grid-template-columns: 1fr 1fr 1fr;
-        }
-        .btn-block {
-            margin-top: 1rem;
-        }
-    }
-`;
+  }
+`
 
-export default Wrapper;
+export default Wrapper
